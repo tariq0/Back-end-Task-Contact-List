1403080eed82e45fa89ef718a8cbcd6f79e1f83e
tariq <tariqabdelghani@gmail.com>
2019-10-04 22:11:01 +0200

    added error handler in index.js, not found middleware also.

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 //
 //
 const express = require('express');
-const isAuthenticated = require('./middlewares/auth-middleware');
-const homeRoutes = require('./routes/home-routes');
 const authRoutes = require('./routes/auth-routes');
 const contactListRoutes = require('./routes/contact-list-routes');
-
+const errorHandler = require('./middlewares/error-middleware');
+const notFound = require('./middlewares/notfound-middleware');
 const port = 4000;
-const sucessMessage = 'server runs on port' + port;
+const serverStartMessage = `server runs on port ${port}`;
 
+// establish db connection
 const dbConnection = require('./database');
 const app = express();
 
 
 app.use(express.json());
-
-app.use('', homeRoutes);
 app.use('/auth', authRoutes);
-
-app.use(isAuthenticated);
 app.use('/contacts', contactListRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => {
-    console.log(sucessMessage);
+    console.log(serverStartMessage);
 });
